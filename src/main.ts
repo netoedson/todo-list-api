cc15072d60301aa94d19d609bf2bce37bb91c7ce
Edson Neto <edsonnetowr@gmail.com>
2021-03-06 18:22:39 -0300

    New: included helmet package

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { token } from './auth/middleware/token.middleware';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.use(helmet());
+  app.use(token);
+
   await app.listen(3000);
 }
 bootstrap();
