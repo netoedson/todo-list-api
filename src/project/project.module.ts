24539bc6f7278b2a8308e7964f0acc46b7cf3f07
Edson Neto <edsonnetowr@gmail.com>
2021-03-06 18:24:43 -0300

    New: started the Project Module

--- src/project/project.module.ts
+++ src/project/project.module.ts
@@ -3,9 +3,10 @@ import { ProjectService } from './project.service';
 import { ProjectController } from './project.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { projectModelsProvider } from './project-models.provider';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-    imports: [MongooseModule.forFeature([...projectModelsProvider])],
+    imports: [AuthModule, MongooseModule.forFeature([...projectModelsProvider])],
   providers: [ProjectService],
   controllers: [ProjectController]
 })
